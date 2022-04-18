9ec1c43a3e80a8b926efdfbd0b0f2268277fee70
Oscar Cederberg <os7138ce-s@student.lu.se>
2022-04-18 22:00:54 +0200

    Add index, remove node_modules

--- themes/cederberg/tailwind.config.js
+++ themes/cederberg/tailwind.config.js
@@ -1,7 +1,26 @@
 module.exports = {
   content: ["content/**/*.md", "layouts/**/*.html"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'title': ['Arial','Helvetica','sans-serif'],
+      'lyrics': ['"Times New Roman"','Times','serif']
+    },
+    extend: {
+      keyframes: {
+        scroll_lp: {
+          from: { transform: 'translateX(0%)'},
+          to: { transform: 'translateX(-100%)'}
+        },
+        scroll_rp: {
+          from: { transform: 'translateX(100%)'},
+          to: { transform: 'translateX(0%)'}
+        },
+      },
+      animation: {
+        scroll_lp: 'scroll_lp 24s linear infinite',
+        scroll_rp: 'scroll_rp 24s linear infinite',
+      },
+    },
   },
   plugins: [],
 }
